ad39eba12e896a21317a8a095d1bed9bade37e83
joemmalatesta <joemmalatesta@gmail.com>
2023-02-03 00:41:42 -0500

    project cards looking better

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -1,17 +1,23 @@
 <script>
-    export let title
-    export let description
-    export let tech
-    export let url
-    export let buttonText = 'Learn more'
+	export let title;
+	export let description;
+	export let tech;
+	export let url;
+	export let buttonText = "Learn more";
 </script>
 
-
 <!-- Maybe make one for smaller screens where its just title and tech with a dropdown. -->
-<div class="container bg-gradient-to-r from-rose-400 to-sky-300 rounded-lg p-5 flex flex-col items-start">
-    <h2 class="text-2xl">{title}</h2>
-    <p class="text-sm md:text-base mb-2 text-neutral-700">{tech}</p>
-    <p class="mb-3 sm:text-lg text-base">{description}</p>
-    <a href="{url}" target="_blank" rel="noreferrer" class="bg-neutral-700 px-4 py-2 rounded-full text-white font-medium hover:bg-indigo-800 transition ease-in-out duration-150">{buttonText}</a>
+<div
+	class="container bg-gradient-to-r from-rose-400 to-sky-300 rounded-lg p-5 flex flex-col items-start"
+>
+	<h2 class="text-2xl">{title}</h2>
+	<p class="text-sm md:text-base mb-2 text-neutral-700">{tech}</p>
+	<p class="mb-3 sm:text-lg text-base">{description}</p>
+	<a
+		href={url}
+		target="_blank"
+		rel="noreferrer"
+		class="bg-neutral-200 px-4 py-2 rounded-full text-black hover:text-white font-medium hover:bg-rose-500 hover:transition ease-in-out shadow-black shadow-md"
+		>{buttonText}</a
+	>
 </div>
-
