74f258061f4479ad942bae34ae63ef1ddfdf0146
Joe Malatesta <joemmalatesta@gmail.com>
2023-02-11 23:48:34 -0500

    section Header component and start of contact

---
+++ src/components/SectionHeader.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let title
+</script>
+
+<h2
+class="bg-gradient-to-r from-gray-900 to-gray-600 bg-clip-text p-5 text-center text-5xl font-extrabold text-transparent lg:text-7xl">
+{title}
+</h2>
--- src/components/contact.svelte
+++ src/components/contact.svelte
@@ -1,7 +1,26 @@
+<script>
+  import SectionHeader from "./SectionHeader.svelte";
+  let name, subject, message
+
+</script>
 <main>
-    <h2
-    class="bg-gradient-to-r from-gray-900 to-gray-600 bg-clip-text p-5 text-center text-5xl font-extrabold text-transparent lg:text-7xl"
->
-    Contact
-</h2>
+    <SectionHeader title={"Contact"} />
+    <form action="/">
+        <h4>Name</h4>
+        <input type="text">
+        <h4>Email</h4>
+        <input type="text">
+        <h4>Subject</h4>
+        <input type="text">
+        <h4>Message</h4>
+        <input type="text">
+        <br>
+        <button type="submit">Send</button>
+    </form>
 </main>
+
+<style>
+    input {
+        @apply outline-double
+    }
+</style>
