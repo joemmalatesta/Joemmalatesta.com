60e10b9078cbfe7429e5f9f983589d5540c6f50e
Joe Malatesta <joemmalatesta@gmail.com>
2023-02-12 14:59:47 -0500

    update headings and contact form

--- src/components/SectionHeader.svelte
+++ src/components/SectionHeader.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <h2
-class="bg-gradient-to-r from-gray-900 to-gray-600 bg-clip-text p-5 text-center text-5xl font-extrabold text-transparent lg:text-7xl">
+class="bg-neutral-800 bg-clip-text p-5 text-center text-5xl font-extrabold text-transparent lg:text-7xl">
 {title}
 </h2>
--- src/components/contact.svelte
+++ src/components/contact.svelte
@@ -10,14 +10,14 @@
     <div class="container flex flex-row justify-center items-center">
     <form action="/" class="mx-3 md:mx-auto">
         <div class="md:flex flex-row">
-            <input type="text" placeholder="Name" class="outline-double p-2 focus:placeholder:opacity-0 transition-all rounded-md m-1 duration-700 w-full md:w-auto">
-            <input type="text" placeholder="Email" class="outline-double p-2 focus:placeholder:opacity-0 transition-all rounded-md m-1 w-full md:w-auto">
+            <input type="text" placeholder="Name" class="outline-double p-2 focus:placeholder:opacity-0 rounded-md m-1 duration-700 w-full md:w-auto">
+            <input type="text" placeholder="Email" class="outline-double p-2 focus:placeholder:opacity-0 rounded-md m-1 w-full md:w-auto">
         </div>
         <div class="md:flex flex-col">
-            <input type="text" placeholder="Subject" class="outline-double p-2 focus:placeholder:opacity-0 transition-all rounded-md m-1 w-full md:w-auto">
-            <input type="text" placeholder="Message" class="outline-double p-2 focus:placeholder:opacity-0 transition-all rounded-md m-1 w-full md:w-auto">
+            <input type="text" placeholder="Subject" class="outline-double p-2 focus:placeholder:opacity-0 rounded-md m-1 w-full md:w-auto">
+            <input type="text" placeholder="Message" class="outline-double p-2 focus:placeholder:opacity-0 rounded-md m-1 w-full md:w-auto">
         </div>
-        <button type="submit" class="p-2 bg-slate-700 w-full rounded-lg" on:click={sendEmail()}>Send</button>
+        <button type="submit" class="p-2 bg-neutral-800 hover:bg-neutral-900 border border-red-200 w-full text-white rounded-lg" on:click={sendEmail()}>Send</button>
     </form>
     </div>
 </main>
