ff95266822069399e171b407254e30bf3d76f8bc
joemmalatesta <joemmalatesta@gmail.com>
2023-02-14 17:03:29 -0500

    front page looking a little better

--- src/components/SectionHeader.svelte
+++ src/components/SectionHeader.svelte
@@ -3,7 +3,7 @@
     export let subtitle = ""
 </script>
 
-<div class="flex flex-col justify-center items-center p-5">
+<div class="flex flex-col justify-center items-center p-5 text-center">
     <h2
     class="bg-neutral-800 bg-clip-text text-center text-5xl py-2 font-extrabold text-transparent lg:text-7xl">
     {title}
