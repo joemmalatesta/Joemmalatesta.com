0d334927a71b3f194839c7c3f117abd2a524d1fe
joemmalatesta <joemmalatesta@gmail.com>
2023-02-14 23:26:10 -0500

    fixed up contact a bit

--- src/components/contact.svelte
+++ src/components/contact.svelte
@@ -1,23 +1,18 @@
 <script>
   import SectionHeader from "./SectionHeader.svelte";
-  let name, subject, message
+  let name, email, message
   function sendEmail(){
     console.log("Email successfully sent!")
   }
 </script>
 <main>
     <SectionHeader title={"Contact"} subtitle={"Say what you gotta say"} />
-    <div class="container flex flex-col justify-center items-center">
-    <form action="/" class="mx-3 md:mx-auto">
-        <div class="md:flex flex-row">
-            <input type="text" placeholder="Name" class="outline-double p-2 focus:placeholder:opacity-0 focus:transition-all rounded-md m-1 w-full md:w-auto">
-            <input type="text" placeholder="Email" class="outline-double p-2 focus:placeholder:opacity-0 rounded-md m-1 w-full md:w-auto">
-        </div>
-        <div class="md:flex flex-col">
-            <input type="text" placeholder="Subject" class="outline-double p-2 focus:placeholder:opacity-0 rounded-md m-1 w-full md:w-auto">
-            <input type="text" placeholder="Message" class="outline-double p-2 focus:placeholder:opacity-0 rounded-md m-1 w-full md:w-auto">
-        </div>
-        <button type="submit" class="p-2 bg-neutral-800 hover:bg-neutral-900 w-full text-white rounded-lg" on:click={sendEmail}>Send</button>
+    <div class="flex justify-center">
+    <form action="/" class="mx-3 flex flex-col lg:w-1/3 w-full">
+            <input type="text" bind:value={name} placeholder="Name" class="ring ring-neutral-700 p-2 focus:outline-none focus:ring-neutral-800 focus:placeholder:opacity-0 rounded-md m-2">
+            <input type="text" bind:value={email} placeholder="Email" class="ring ring-neutral-700 p-2 focus:outline-none focus:ring-neutral-800 focus:placeholder:opacity-0 rounded-md m-2">
+            <input type="text" bind:value={message} placeholder="Message" class="ring ring-neutral-700 p-2 focus:outline-none focus:ring-neutral-800 focus:placeholder:opacity-0 rounded-md m-2 h-40 pt-2">
+          <button type="submit" class="p-2 bg-rose-400 hover:bg-rose-500 text-white rounded-lg scale-95" on:click={sendEmail}>Get in Touch</button>
     </form>
     </div>
 </main>
