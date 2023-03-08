21934e452c90332da5de63f41e1746f3f10852ed
joemmalatesta <joemmalatesta@gmail.com>
2023-03-07 23:52:19 -0500

    progress on the carousel

--- src/components/blog2.svelte
+++ src/components/blog2.svelte
@@ -5,28 +5,64 @@
 	import SectionHeader from "./SectionHeader.svelte";
 	import { crossfade, fade } from "svelte/transition";
 	import BlogCard2 from "./blogCard2.svelte";
-	export let postTypes;
-	export let posts;
-	let active = 0;
-	export let quotes;
+	import { flip } from "svelte/animate";
+  export let postTypes;
+  export let posts;
+  let active = 1;
+  export let quotes;
+
+  let activePosts = posts[0].slice(0, 3);
+
+  function handleClick(index) {
+    console.log(activePosts[0].frontmatter.title);
+    if (index == 1) {
+      return;
+    }
+    if (index == 0) {
+      rotateRight();
+    }
+    if (index == 2) {
+      rotateLeft();
+    }
+  }
+
+  function rotateLeft() {
+    const temp = activePosts[0];
+    for (let i = 0; i < activePosts.length - 1; i++) {
+      activePosts[i] = activePosts[i + 1];
+    }
+    activePosts[activePosts.length - 1] = temp;
+
+  }
+
+  function rotateRight() {
+    const temp = activePosts[activePosts.length - 1];
+    for (let i = activePosts.length - 1; i > 0; i--) {
+      activePosts[i] = activePosts[i - 1];
+    }
+    activePosts[0] = temp;
+
+  }
 </script>
 
 <main>
-	<SectionHeader
-		title={"Blog"}
-		subtitle={"A place for my thoughts about things"}
-	/>
+  <SectionHeader title={"Blog"} subtitle={"A place for my thoughts about things"} />
 
-	<div class="flex justify-around items-center">
-		{#each posts[active].slice(0, 4) as post}
-			<BlogCard2
-				client:load
-				title={post.frontmatter.title}
-				description={post.frontmatter.cardDescription}
-				date={post.frontmatter.pubDate}
-				url={post.url}
-				image={post.frontmatter.heroImage}
-			/>
-		{/each}
-	</div>
+  <div class="flex justify-center items-center">
+    {#each activePosts as post, index}
+    <div
+      class="-mx-7 {index == 1 ? 'scale-105 z-50' : 'scale-90 opacity-70'}"
+      on:click={() => handleClick(index)}
+      on:keypress={() => handleClick(index)}
+    >
+      <BlogCard2
+        title={post.frontmatter.title}
+        description={post.frontmatter.cardDescription}
+        date={post.frontmatter.pubDate}
+        url={post.url}
+        image={post.frontmatter.heroImage}
+      />
+    </div>
+    {/each}
+  </div>
 </main>
--- src/components/blogCard2.svelte
+++ src/components/blogCard2.svelte
@@ -2,12 +2,15 @@
     export let title, description, date, url, image;
 </script>
 
-
-<div class="outline flex flex-col w-60 h-88">
+<!-- <a href={url} class="p-2"> -->
+<div class="outline flex flex-col w-60 h-88 rounded-lg">
     <div>
-        <img src={image} alt="{title} Image">
+        <img src={image} alt="{title} Image" class="">
     </div>
-    <div class="flex flex-col">
-
+    <div class="flex flex-col bg-rose-200 p-4">
+        <p>{date}</p>
+        <h3>{title}</h3>
+        <p>{description}</p>
     </div>
 </div>
+<!-- </a> -->
