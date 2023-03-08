5ba43dd8fc73abcb28e21b58e1f2a1204a572509
Joe Malatesta <joemmalatesta@gmail.com>
2023-03-07 19:18:39 -0500

    blog starting to change

---
+++ src/components/blog2.svelte
@@ -0,0 +1,32 @@
+<script>
+	import BaseHead from "../components/BaseHead.astro";
+	import Footer from "../components/Footer.astro";
+	import BlogCard from "../components/blogCard.svelte";
+	import SectionHeader from "./SectionHeader.svelte";
+	import { crossfade, fade } from "svelte/transition";
+	import BlogCard2 from "./blogCard2.svelte";
+	export let postTypes;
+	export let posts;
+	let active = 0;
+	export let quotes;
+</script>
+
+<main>
+	<SectionHeader
+		title={"Blog"}
+		subtitle={"A place for my thoughts about things"}
+	/>
+
+	<div class="flex justify-around items-center">
+		{#each posts[active].slice(0, 4) as post}
+			<BlogCard2
+				client:load
+				title={post.frontmatter.title}
+				description={post.frontmatter.cardDescription}
+				date={post.frontmatter.pubDate}
+				url={post.url}
+				image={post.frontmatter.heroImage}
+			/>
+		{/each}
+	</div>
+</main>
---
+++ src/components/blogCard2.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let title, description, date, url, image;
+</script>
+
+
+<div class="outline flex flex-col w-60 h-88">
+    <div>
+        <img src={image} alt="{title} Image">
+    </div>
+    <div class="flex flex-col">
+
+    </div>
+</div>
