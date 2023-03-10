d97761b9f829a1f3b9c25f6d5008639172184929
Joe Malatesta <joemmalatesta@gmail.com>
2023-03-09 19:35:39 -0500

    new project cards

---
+++ src/components/project2.svelte
@@ -0,0 +1,86 @@
+<script>
+	import ProjectCard from "./ProjectCard.svelte";
+	import ProjectCard2 from "./projectCard2.svelte";
+	import SectionHeader from "./SectionHeader.svelte";
+	import { slide, fade, crossfade, fly } from "svelte/transition";
+	import { flip } from "svelte/animate";
+
+	let active = null;
+	const projects = [
+		{
+			id: 0,
+			title: "Auto Typer",
+			tech: "Python, Selenium, MatPlotLib",
+			description:
+				"Auto complete a typing test at a specified WPM, taking into account computational delays",
+			url: "https://github.com/joemmalatesta/Auto-Typer",
+			buttonText: "Learn more",
+			image: "projects/auto-typer.jpg",
+		},
+		{
+			id: 1,
+			title: "Personal Website V1",
+			tech: "HTML, CSS, Bootstrap, PHP, MySQL",
+			description:
+				"The first iteration of my website! My introduction to front end development.",
+			url: "https://dev.joemmalatesta.com/",
+			buttonText: "Visit old site",
+			image: "projects/old-website.jpg",
+		},
+		{
+			id: 2,
+			title: 'Discord "Yvy" Bot',
+			tech: "Python, REST APIs, MongoDB",
+			description:
+				"Commands return concise information regarding osu! plays, and players.",
+			url: "https://github.com/joemmalatesta/yvy-discord-bot",
+			buttonText: "Learn more",
+			image: "projects/yvy-bot.jpg",
+		},
+		{
+			id: 3,
+			title: "CaptChart",
+			tech: "Javascript, Chartjs, Bootstrap",
+			description:
+				"Captcha completed by correctly identifying parts of a linear graph",
+			url: "https://joemmalatesta.github.io/CapChart/",
+			buttonText: "Live demo",
+			image: "projects/capchart.jpg",
+		},
+	];
+</script>
+
+<SectionHeader
+	title={"Projects"}
+	subtitle={"blood, sweat, and carpal tunnel has lead me to this"}
+/>
+
+<div class="flex justify-around flex-wrap">
+	{#each projects as project, index (project.id)}
+		<div>
+			<ProjectCard2
+				title={project.title}
+				tech={project.tech}
+				description={project.description}
+				url={project.url}
+				buttonText={project.buttonText}
+				image={project.image}
+			/>
+		</div>
+	{/each}
+</div>
+<!-- Maybe use for phone sizes.  -->
+
+<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:hidden">
+	{#each projects as project}
+		<ProjectCard
+			title={project.title}
+			tech={project.tech}
+			description={project.description}
+			url={project.url}
+			buttonText={project.buttonText}
+			image={project.image}
+			phoneScreen={true}
+		/>
+	{/each}
+</div>
---
+++ src/components/projectCard2.svelte
@@ -0,0 +1,40 @@
+<script>
+    export let title, description, tech, url, image;
+</script>
+
+<!-- <a href={url} class="p-2"> -->
+<!-- <div class=" flex flex-col w-96 h-96 relative">
+    <div class="h-full">
+        <img src={image} alt="{title} Image" class="object-cover rounded-t-lg">
+    </div>
+    <div class="flex flex-col bg-rose-200 p-4 h-1/2 rounded-b-lg absolute align-bottom">
+        <p>{tech}</p>
+        <h3>{title}</h3>
+        <p>{description}</p>
+    </div>
+</div> -->
+<!-- </a> -->
+
+
+<div class="relative">
+    <img src={image} alt="" class="w-[480px] h-[270px]">
+    <div class="absolute inset-0 bg-gray-900 opacity-75 rounded-xl" />
+    <div class="absolut botto">
+        <p>{tech}</p>
+        <h3 class="text-4xl font-bold text-white px-2">{title}</h3>
+        <p>{description}</p>
+
+    </div>
+</div>
+
+
+
+<!-- <div
+		class="relative h-36 bg-cover bg-center hover:scale-105 transition-all duration-300 active:scale-95 rounded-xl"
+		style="background-image: url({image});"
+	>
+		<div class="absolute inset-0 bg-gray-900 opacity-75 rounded-xl" />
+		<div class="absolute inset-0 flex items-center justify-center">
+			<h3 class="text-4xl font-bold text-white text-center px-2">{title}</h3>
+		</div>
+	</div> -->
