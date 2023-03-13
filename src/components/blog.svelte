15b84aaa3e89e92488e591a798ad7313bb1d9966
joemmalatesta <joemmalatesta@gmail.com>
2023-03-12 23:32:21 -0400

    something dumb.

--- src/components/blog.svelte
+++ src/components/blog.svelte
@@ -3,7 +3,7 @@
 	import Footer from "../components/Footer.astro";
 	import BlogCard from "../components/blogCard.svelte";
 	import SectionHeader from "./SectionHeader.svelte";
-	import { crossfade, fade } from "svelte/transition";
+	import { crossfade, fade, slide } from "svelte/transition";
 	export let postTypes;
 	export let posts;
 	let active = 0;
@@ -65,8 +65,8 @@
 			<!-- Blog part -->
 			<div
 				class="flex flex-col items-start justify-center"
-				transition:fade|local
 			>
+			
 				{#each posts[active].slice(0, 4) as post}
 					<BlogCard
 						client:load
@@ -77,17 +77,65 @@
 						image={post.frontmatter.heroImage}
 					/>
 				{/each}
+			
 			</div>
 		</div>
 
 		<!-- Quote / other portion -->
 		<!-- Maybe should do a specific signup for newsletter and the quote next to it so it doesn't look so shit -->
-		<div class="lg:w-2/5 flex items-center mx-5 bg-gradient-to-br from-rose-200 via-rose-300 to-rose-400 p-6 rounded-tr-full rounded-bl-full">
+
+
+		<div class="relative w-1/3 justify-center items-center hidden lg:flex">
+			<div class="bg-gradient-to-tl from-rose-400/70 to-blue-400/70 blur-2xl absolute w-96 outline h-96 rounded-full xl:translate-x-32 translate-x-24 z-40 rotating" />
+			<p class="absolute lg:text-3xl xl:text-4xl font-serif xl:w-2/3 z-50 font-semibold quote ">{quotes[active]}</p>
+			
+		</div>
+		<!-- <div class="lg:w-2/5 flex items-center mx-5 bg-gradient-to-br from-rose-200 via-rose-300 to-rose-400 p-6 rounded-tr-full rounded-bl-full blur-sm">
 			<p
 				class="md:text-4xl p-6 font-serif text-2xl"
 			>
 				{quotes[active]}
 			</p>
-		</div>
+		</div> -->
 	</div>
 </main>
+
+
+
+<style>
+	@-webkit-keyframes rotating /* Safari and Chrome */ {
+  from {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  to {
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes rotating {
+  from {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  to {
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+.rotating {
+  -webkit-animation: rotating 6s linear infinite;
+  -moz-animation: rotating 6s linear infinite;
+  -ms-animation: rotating 6s linear infinite;
+  -o-animation: rotating 6s linear infinite;
+  animation: rotating 6s linear infinite;
+}
+</style>
